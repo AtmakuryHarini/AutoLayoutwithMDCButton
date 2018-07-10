d8877c7e1080bd31cab377813e3d0a8173985a6c
Atmakury Harish <happlabs@gmail.com>
2018-07-10 20:26:30 +0800

    added margin 15 above and below along with MDC button.

--- AutoLayoutTblviewEx/ViewController.swift
+++ AutoLayoutTblviewEx/ViewController.swift
@@ -5,194 +5,151 @@
 //  Created by Atmakury Harish on 5/7/18.
 //  Copyright © 2018 Atmakury Harini. All rights reserved.
 //
-
 import UIKit
-import Alamofire
 import MaterialComponents.MaterialButtons
 import MaterialComponents.MaterialButtons_ButtonThemer
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,MDCInkViewDelegate {
+    private lazy var customTblView :UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = true
+        tableView.register(tableCell.self, forCellReuseIdentifier: "customCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        //u need to include these 2 to make the file dynamic
+        tableView.estimatedRowHeight = 70
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.separatorStyle = .none //putting UITableViewCellSeparatorStyle infront is uncessary
+        return tableView
+    }()
+    
+    private lazy var titlelbl:UILabel = {
+        let label = UILabel()
+        label.text = "TYPES OF FOOD"
+        label.font = UIFont(name: fontName, size: 20.0)
+        label.textAlignment = NSTextAlignment.center
+        label.textColor =  UIColor.black
+        
+        return label
+    }()
+    private lazy var saveButton:MDCButton =
+    {
+    let button = MDCButton()
+    let buttonScheme = MDCButtonScheme()
+    MDCTextButtonThemer.applyScheme(buttonScheme, to: button)
+    button.setTitleColor(UIColor.green, for: .normal)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("Button Clicked", for: .normal)
+    button.inkColor = UIColor.white
+    button.setBackgroundColor(UIColor.blue)
+    button.titleLabel?.textColor = UIColor.white
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,MDCInkTouchControllerDelegate{
-    private var customTblView :UITableView!
-    let saveButton = MDCButton()
-    private var titlelbl = UILabel()
+    return button
+    }()
+
     let fontName  = "AvenirNext-DemiBold"
-    private var dictTblData =  [[String :[String]]]()
-    private var delegate = MDCInkTouchControllerDelegate.self
-   
+    private var dictTblData:[[String :[String]]] = [["title":["Vegetarian"],"rows":["Butter","yogurt","milk"],"type":["Plant and Dairy"],"price":["$7","$9","$3"]],
+                                                    ["title":["Vegan"],"rows":["orange","Apple","Vegeatables"],"type":["Plant Based"],"price":["$20","$25","$34"]],
+                                                    ["title":["Non-Vegetarian"],"rows":["Chicken","Lamb","Pork"],"type":["Animal Based"],"price":["$500","$340","$303"]]]
     //-----------------------------------------------------------
-    struct courses:Decodable {
-    let name: String
-    let number_of_lessons:Int
-    let id :Int
-    }
-    private var dictTblData2 =  [courses]()
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        SetupView()     //view setup
-        LayoutSubView() //auto layout
-
-         //tableview data
-        dictTblData = [["title":["Vegetarian"],"rows":["Butter","yogurt","milk"],"type":["Plant and Dairy"],"price":["$7","$9","$3"]],
-            ["title":["Vegan"],"rows":["orange","Apple","Vegeatables"],"type":["Plant Based"],"price":["$20","$25","$34"]],
-            ["title":["Non-Vegetarian"],"rows":["Chicken","Lamb","Pork"],"type":["Animal Based"],"price":["$500","$340","$303"]]]
-        customTblView.reloadData()
+        view.backgroundColor = .white
+        setupView()     //view setup
+        layoutConstraints() //auto layout
+       
     }
-    //------------------------------------------------------------
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-  
+    
     //------------------------------------------------------------
     //MARK: CUSTOM FUNCTIONS
     //------------------------------------------------------------
     
-    func SetupView()
-    {
-    //tableView
-    customTblView = UITableView(frame: CGRect(x:0,y:150,width:100,height:100))
-    customTblView.register(UINib(nibName: "viewControllerCell", bundle: nil), forCellReuseIdentifier: "customCell")
-    customTblView.dataSource = self
-    customTblView.delegate = self
-    self.view.addSubview(customTblView)
-    customTblView.separatorStyle = UITableViewCellSeparatorStyle.none //remove lines
+    lazy var stackView:UIStackView = {
+        let stackView:UIStackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(titlelbl)
+        stackView.addArrangedSubview(customTblView)
+        stackView.addArrangedSubview(saveButton)
+        return stackView
+    }()
     
-    //Title Label
-    titlelbl.text = "TYPES OF FOOD"
-    titlelbl.font = UIFont(name: fontName, size: 20.0)
-    titlelbl.textAlignment = NSTextAlignment.center
-    titlelbl.textColor =  UIColor.black
-    self.view.addSubview(titlelbl)
-        
-     // MDC ink save button
-    let buttonScheme = MDCButtonScheme()
-    MDCTextButtonThemer.applyScheme(buttonScheme, to: saveButton)
-    saveButton.setTitleColor(UIColor.green, for: .normal)
-    saveButton.translatesAutoresizingMaskIntoConstraints = false
-    saveButton.setTitle("Button Clicked", for: .normal)
-    self.view.addSubview(saveButton)
-    let inkTouchController = MDCInkTouchController(view: saveButton)
-    inkTouchController.delegate = ViewController()
-    inkTouchController.addInkView()
-    saveButton.inkColor = UIColor.white
-    saveButton.setBackgroundColor(UIColor.blue)
-    saveButton.titleLabel?.textColor = UIColor.white
+    func setupView()
+    {
+        view.addSubview(stackView)
     }
     
-    func LayoutSubView()
+    func layoutConstraints()
     {
-    //tableview
-    customTblView.translatesAutoresizingMaskIntoConstraints = false
-        customTblView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true //top
-    customTblView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true //bottom
         
-    customTblView.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true//width
-        
-   customTblView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true //leading
-  customTblView.trailingAnchor.constraint(equalTo:self.view.trailingAnchor, constant: 0).isActive = true //trailing
-        
-    //Title Lable
-    titlelbl.translatesAutoresizingMaskIntoConstraints = false
-    titlelbl.topAnchor.constraint(equalTo: self.view.topAnchor, constant:80).isActive = true //top
-    titlelbl.bottomAnchor.constraint(equalTo:customTblView.topAnchor, constant:0).isActive = true //bottom
-        
-    titlelbl.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true //width
-    titlelbl.heightAnchor.constraint(equalToConstant:20).isActive = true //bottom
-    
-    titlelbl.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true //trailing
-    titlelbl.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true //leading
-        
-    //save button
-    saveButton.translatesAutoresizingMaskIntoConstraints = false
-    saveButton.topAnchor.constraint(equalTo: customTblView.bottomAnchor, constant:0).isActive = true //top
-        
-    saveButton.widthAnchor.constraint(equalToConstant: 200).isActive = true //width
-    saveButton.heightAnchor.constraint(equalToConstant:40).isActive = true //bottom
-    saveButton.centerXAnchor.constraint(equalTo: customTblView.centerXAnchor).isActive = true //centerAxis
-    
-    }
-    func BNdecodedData(from urlstring: String)
-    {
-        var  responseData = [courses]()
-        // Alamofire request
-        Alamofire.request(urlstring)
-            .responseJSON { response in
-                guard response.result.error == nil else {
-                    print("error in dowloading")
-                    print(response.result.error!)
-                    return
-                }
-                
-                // Decoding
-                do {
-                    let decoder = JSONDecoder()
-                    responseData = try decoder.decode([courses].self, from: response.data!)
-                    print("response is\(responseData[0].id)")
-                } catch {
-                    print(error)
-                }
-        }
-        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true //top
+        stackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true //bottom
+        stackView.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true//width
+        stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant:0).isActive = true
+         stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor,constant:0).isActive = true
         
     }
-
     //------------------------------------------------------------
     //MARK: TABLEVIEW DATA SOURCE AND DELEGATE
     //------------------------------------------------------------
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard dictTblData[section]["rows"]?.count != nil else {
-        return 0
+            return 0
         }
         return (dictTblData[section]["rows"]?.count)!
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as? viewControllerCell
-    guard (dictTblData[indexPath.section]["rows"]?[indexPath.row]) != nil || (dictTblData[indexPath.section]["price"]?[indexPath.row]) != nil || (dictTblData[indexPath.section]["type"]?[0]) != nil else {
-    cell?.cellLabel1.text = "Label1"
-    cell?.cellLabel2.text = "Label2"
-    cell?.cellLabel3.text = "Label3"
-    return cell!
-     }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as? tableCell
         
-   cell?.cellLabel1.text = (dictTblData[indexPath.section]["rows"]?[indexPath.row])!;
-   cell?.cellLabel2.text = (dictTblData[indexPath.section]["type"]![0])
-   cell?.cellLabel3.text = (dictTblData[indexPath.section]["price"]?[indexPath.row])!;
-   return cell!
-   }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 50
+        
+        //this is good that you are guarding against nil here to stop potential bad text
+        //but what happens if only 1 of the dataSource rows are empty? e.g. just rows
+        //would you need to see the rest?
+        guard (dictTblData[indexPath.section]["rows"]?[indexPath.row]) != nil || (dictTblData[indexPath.section]["price"]?[indexPath.row]) != nil || (dictTblData[indexPath.section]["type"]?[0]) != nil else {
+            
+            cell?.cellLabel1.text = "Label1"
+            cell?.cellLabel2.text = "Label2"
+            cell?.cellLabel3.text = "Label3"
+            return cell!
+        }
+        
+        cell?.cellLabel1.text = (dictTblData[indexPath.section]["rows"]?[indexPath.row])!
+        cell?.cellLabel2.text = (dictTblData[indexPath.section]["type"]![0])
+        cell?.cellLabel3.text = (dictTblData[indexPath.section]["price"]?[indexPath.row])!
+        return cell!
     }
     
-    //-----------------------------------------------------------
-    //MARK: MDC DELEGATES
-    //------------------------------------------------------------
-    
-    func inkTouchController(_ inkTouchController: MDCInkTouchController, shouldProcessInkTouchesAtTouchLocation location: CGPoint) -> Bool {
-        return true
-    }
+    //no need just use autolayout
+    //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    //    return 40
+    //    }
     //-----------------------------------------------------------
     //MARK: TABLEVIEW SECTION
     //------------------------------------------------------------
     func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-    return dictTblData.count
+        return dictTblData.count
     }
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-    return dictTblData.count
+        return dictTblData.count
     }
-
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 55;
     }
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
     {
         //headerview
-        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 30))
-        headerView.backgroundColor = UIColor.blue
+        
+        //unecessary framing
+        let headerView = UIView()
+        headerView.backgroundColor =  UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
         
         //header label
         let headerlbl = UILabel()
@@ -213,9 +170,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             ])
         return headerView
     }
+    
     //-----------------------------------------------------------
-
- 
-
-
 }
--- AutoLayoutTblviewEx/viewControllerCell.swift
+++ AutoLayoutTblviewEx/viewControllerCell.swift
@@ -5,41 +5,78 @@
 //  Created by Atmakury Harish on 5/7/18.
 //  Copyright © 2018 Atmakury Harini. All rights reserved.
 //
-
 import UIKit
 
-class viewControllerCell: UITableViewCell {
-
+class tableCell: UITableViewCell {
     
-    @IBOutlet weak var cellLabel1: UILabel!
-    @IBOutlet weak var cellLabel2: UILabel!
-    @IBOutlet weak var cellLabel3: UILabel!
-    //----------------------------------------------------------
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    //----------------------------------------------------------
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-         
-        // Configure the view for the selected state
-    }
-   //----------------------------------------------------------
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)!
-    }
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    lazy var cellLabel1:UILabel = {
+        let label:UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.left
+        label.font = UIFont(name: label.font.fontName, size: 14)
+        return label
+    }()
+    
+    lazy var cellLabel2:UILabel = {
+        let label:UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.center
+        label.font = UIFont(name: label.font.fontName, size: 14)
+        return label
+    }()
+
+    lazy var cellLabel3:UILabel = {
+        let label:UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.right
+        label.font = UIFont(name: label.font.fontName, size: 14)
+        return label
+    }()
+    
+    lazy var stack:UIStackView = {
+        let stack:UIStackView = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         
-        // Cell dimension constraints code
-       
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        print("test")
+        setupViews()
+        setupConstraints()
     }
-   
     
-    //----------------------------------------------------------
-
+    func setupViews(){
+        
+        contentView.addSubview(stack)
+        stack.addArrangedSubview(cellLabel1)
+        let spacer1:UIView = UIView()
+        spacer1.translatesAutoresizingMaskIntoConstraints = true
+        spacer1.heightAnchor.constraint(equalToConstant: 20)
+        //stack.addArrangedSubview(spacer1)
+        stack.addArrangedSubview(cellLabel2)
+        let spacer2:UIView = UIView()
+        spacer2.translatesAutoresizingMaskIntoConstraints = true
+        spacer2.heightAnchor.constraint(equalToConstant: 20)
+        //stack.addArrangedSubview(spacer2)
+        stack.addArrangedSubview(cellLabel3)
+        
+    }
+    
+    func setupConstraints(){
+        stack.topAnchor.constraint(equalTo: contentView.topAnchor,constant:15).isActive = true
+        stack.leadingAnchor.constraint(equalTo:contentView.leadingAnchor, constant: 5).isActive = true
+        stack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 5).isActive = true
+        stack.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant:-10).isActive = true
+        stack.alignment = UIStackViewAlignment.fill
+        stack.distribution = UIStackViewDistribution.fillEqually
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
